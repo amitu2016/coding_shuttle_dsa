7f771ac84012b6ab476d2308a2dc0c57c39e27a8
Amit Upadhyay <amitu2016@gmail.com>
2024-04-21 16:29:50 +0530

    Rat in a maze problem

--- src/Backtracking/RatInAMaze.java
+++ src/Backtracking/RatInAMaze.java
@@ -13,6 +13,8 @@ public class RatInAMaze {
         int m = mat[0].length;
 
         boolean vis[][] = new boolean[n][m];
+        vis[0][0] = true;
+        ratInAMaze(mat, vis, 0, 0, "", n, m);
     }
 
     public static boolean isValid(int i, int j, int mat[][], boolean vis[][], int n, int m){
@@ -34,6 +36,27 @@ public class RatInAMaze {
             vis[i+1][j] = true;
             ratInAMaze(mat, vis, i+1, j, path+'D', n, m);
             vis[i+1][j] = false;
+        }
+
+        //Left
+        if (isValid(i, j -1, mat, vis, n, m)){
+            vis[i][j-1] = true;
+            ratInAMaze(mat, vis, i, j-1, path+'L', n, m);
+            vis[i][j-1] = false;
+        }
+
+        //Right
+        if (isValid(i, j+1, mat, vis, n, m)){
+            vis[i][j+1] = true;
+            ratInAMaze(mat, vis, i, j+1, path+'R', n, m);
+            vis[i][j+1] = false;
+        }
+
+        //Up
+        if (isValid(i-1, j, mat, vis, n, m)){
+            vis[i-1][j] = true;
+            ratInAMaze(mat, vis, i+1, j, path+'U', n, m);
+            vis[i-1][j] = false;
         }
 
 
